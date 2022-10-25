5566eae25d87fe8cd05b44c251ae171f64d5d9f5
operador <operador@CCA5-07>
2022-10-25 15:15:05 -0500

    Agregar archivos de proyecto.

---
+++ Tec-Ejemplo/Hubs/GameHub.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tec_Ejemplo.Hubs
+{
+    public class Glass
+    {
+        public string ConnectionId { get; set; }
+        public int GameNumber { get; set; }
+    }
+
+    public class GameHub : Hub
+    {
+        private static List<Glass> GameClientes = new List<Glass>();
+        public override Task OnConnectedAsync()
+        {
+
+            if (!GameClientes.Select(client => client.ConnectionId)
+                .Contains(Context.ConnectionId))
+                GameClientes.Add(new Glass
+                {
+                    ConnectionId =
+                    Context.ConnectionId,
+                    GameNumber = 100
+                }
+                );
+            Clients.All.SendAsync("ReceiveData", GameClientes);
+
+            return base.OnConnectedAsync();
+
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            if (GameClientes.Select(Clients => Clients.ConnectionId).Contains(Context.ConnectionId))
+                GameClientes
+                    .Remove(GameClientes
+                    .SingleOrDefault(Clients => Clients.ConnectionId == Context.ConnectionId)
+                    );
+
+
+            return base.OnDisconnectedAsync(exception);
+        }
+
+        public void play()
+        {
+            var currentClient = GameClientes.SingleOrDefault(Clients => Clients.ConnectionId == Context.ConnectionId).GameNumber--;
+
+            if (currentClient <= 0)
+            {
+                Clients.All.SendAsync("ReceiveGameOver", "Game Over!!");
+            }
+            Clients.All.SendAsync("ReceiveData", GameClientes);
+
+        }
+
+
+
+
+    }
+}
---
+++ Tec-Ejemplo/wwwroot/js/game.js
@@ -0,0 +1,34 @@
+﻿
+var connection =
+    new signalR.HubConnectionBuilder().withUrl("/gameHub").build();
+
+connection.start()
+    .then(() => {
+        console.log('Clinete Conectado');
+    })
+
+connection.on("ReceiveData", (clients) => {
+
+    console.table(clients);
+
+    var container = document.getElementById("gameContainer")
+    container.innerHTML = ` `;
+    clients.forEach((item) => {
+        var div = document.createElement("div");
+        //https://file.io/fEektd5DwwJz
+        div.innerHTML = ` 
+            <div class="glass-container" onclick="play()">
+            <img src="/img/glass.png"/>
+            <div class="glass" style="height:${item.gameNumber}%"></div >
+            </div>
+            `;
+        container.appendChild(div);
+    })
+
+})
+
+function play() {
+    connection.invoke("play")
+}
+//connection.on // Funciona para recibir mensajes
+//connection.invoke() //Enviar Mensajes
